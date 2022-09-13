aa3dfefb7ef911b93db8297f68aa2ee63cb37fdc
Toby <newtobycm@gmail.com>
2022-09-12 17:40:12 -0700

    ayy dev log let's goo

---
+++ index.js
@@ -0,0 +1,18 @@
+// 10 Sep 2022
+// I have a better understanding of the question mark (?) in javascript
+
+// condition ? run when true : run when false
+
+10 === 15 ? console.log("yeah 10 is larger than 15") : console.log("nah 15 is bigger than 10") // output: nah 15 is bigger than 10
+
+// 11 Sep 2022
+// I learned that you can use .constructor to see what object type it is
+
+const example_array = ["a", "z", 14, true]
+
+console.log(example_array.constructor) // output: Array function
+
+console.log(example_array.constructor === Array) // output: true
+console.log(example_array.constructor === Number) // output: false
+
+// js is kinda cool
